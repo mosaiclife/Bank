6222c3473f8e4697ac9989f25ee312501c06682b
mosaiclife <mosaiclife@naver.com>
2011-12-28 10:24:13 +0900

    Initial Commit

---
+++ Bank/main.c
@@ -0,0 +1,18 @@
+//
+//  main.c
+//  Bank
+//
+//  Created by EunPyo Hong on 11. 12. 28..
+//  Copyright 2011년 __MyCompanyName__. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main (int argc, const char * argv[])
+{
+
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
+
